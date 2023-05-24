f06706591749d8032307626395444ff9c42b42e3
Deniel Pereira <deniel.pereira@contabilizei.com.br>
2023-05-24 13:57:47 -0300

    feat: add heroku configs

---
+++ index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
